409c7b0e955a0691328e4ae735e9c59c99068424
Victoria Viana <victoriagarcia963@gmail.com>
2022-04-08 19:37:43 -0300

    fix: card component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import CartProduct from "../CartProduct/CartProduct";
+import "./Cart.css";
 
-const Cart = ({ currentSale }) => {
+const Cart = ({ currentSale, setCurrentSale, removeProduct }) => {
   const totalPrice = currentSale.reduce((acc, currentValue) => {
     return acc + Number(currentValue.price);
   }, 0);
@@ -12,21 +13,38 @@ const Cart = ({ currentSale }) => {
     });
   };
 
+  const removeAllProduct = () => {
+    setCurrentSale([]);
+  };
+
   return (
-    <div>
-      <div>
+    <div className="cartContainer">
+      <div className="topCart">
         <h1>Carrinho de Compras</h1>
       </div>
-      <ul>
-        {currentSale.map((sale, index) => (
-          <CartProduct sale={sale} key={index} />
-        ))}
-      </ul>
-      <div>
-        <p>Total</p>
-        <span>{formatCurrency(totalPrice)}</span>
-        <button>Remover Todos</button>
-      </div>
+      {currentSale.length >= 1 ? (
+        <>
+          <ul className="middleCart">
+            {currentSale.map((sale, index) => (
+              <CartProduct
+                sale={sale}
+                key={index}
+                removeProduct={removeProduct}
+              />
+            ))}
+            <div className="bottomCart">
+              <p>Total</p>
+              <span>{formatCurrency(totalPrice)}</span>
+              <button onClick={removeAllProduct}>Remover Todos</button>
+            </div>
+          </ul>
+        </>
+      ) : (
+        <div className="textCart">
+          <p>Sua sacola está vazia</p>
+          <span>Adicione itens</span>
+        </div>
+      )}
     </div>
   );
 };
