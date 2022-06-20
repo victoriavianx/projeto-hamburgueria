1babd5080879e632e1c363ee03f9324cea637fce
Victoria Viana <victoriagarcia963@gmail.com>
2022-06-20 09:49:37 -0300

    feat: add providers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Providers from "./providers/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
   document.getElementById("root")
 );
