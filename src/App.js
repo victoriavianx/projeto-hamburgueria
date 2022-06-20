bcbeabbb24d4e4894fedb6a7bad5c55ecfb17725
Victoria Viana <victoriagarcia963@gmail.com>
2022-06-20 09:47:54 -0300

    update: app.js organization

--- src/App.js
+++ src/App.js
@@ -1,78 +1,18 @@
-import { useState, useEffect } from "react";
 import { GlobalStyles } from "./styles/global";
-import Header from "./components/Header/Header";
-import ProductsList from "./components/ProductsList/ProductsList";
-import Cart from "./components/Cart/Cart";
-import "./App.css";
+import Home from "./pages/home";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [currentSale, setCurrentSale] = useState([]);
-
-  useEffect(() => {
-    async function getProducts() {
-      const response = await fetch(
-        "https://hamburgueria-kenzie-json-serve.herokuapp.com/products"
-      );
-
-      const dataResponse = await response.json();
-
-      setProducts(dataResponse);
-      setFilteredProducts(dataResponse);
-    }
-
-    getProducts();
-  }, []);
-
-  const showProducts = (searchInput) => {
-    const filterProducts = products.filter((product) => {
-      const { name, category } = product;
-      const productName = name.toLowerCase();
-      const productCategory = category.toLowerCase();
-      const search = searchInput.toLowerCase();
-
-      return productName.includes(search) || productCategory.includes(search);
-    });
-
-    setFilteredProducts(filterProducts);
-  };
-
-  const removeProduct = (removeItem) => {
-    const removedProduct = currentSale.filter(
-      (product) => product !== removeItem
-    );
-
-    setCurrentSale(removedProduct);
-  };
-
-  const handleClick = (productId) => {
-    const findProductId = products.find((product) => productId === product.id);
-
-    setCurrentSale([...currentSale, findProductId]);
-  };
-
   return (
     <>
       <GlobalStyles />
-      <header className="topContainer">
-        <Header showProducts={showProducts} />
-      </header>
-      <main className="mainContainer">
-        <section className="sectionContainer">
-          <ProductsList
-            filteredProducts={filteredProducts}
-            handleClick={handleClick}
-          />
-        </section>
-        <aside className="asideContainer">
-          <Cart
-            currentSale={currentSale}
-            setCurrentSale={setCurrentSale}
-            removeProduct={removeProduct}
-          />
-        </aside>
-      </main>
+      <Home />
+      {/* <aside className="asideContainer">
+        <Cart
+          currentSale={currentSale}
+          setCurrentSale={setCurrentSale}
+          removeProduct={removeProduct}
+        />
+      </aside> */}
     </>
   );
 };
